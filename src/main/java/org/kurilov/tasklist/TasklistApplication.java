b69bf5dbcdb943dfe5da2b870d31472c72c34745
space125 <kurilov@list.ru>
2023-10-19 10:33:26 +0300

    TL-0: Created skeleton Spring boot project

---
+++ src/main/java/org/kurilov/tasklist/TasklistApplication.java
@@ -0,0 +1,13 @@
+package org.kurilov.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasklistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kurilov/tasklist/TasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kurilov.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
