c553f3fac6fe881d8275560115438b2a67ac5d14
space125 <kurilov@list.ru>
2023-10-19 16:19:43 +0300

    TL-0: Created skeleton Spring boot project

--- src/main/java/org/kurilov/tasklist/TasklistApplication.java
+++ src/main/java/org/kurilov/tasklist/TasklistApplication.java
@@ -2,8 +2,10 @@ package org.kurilov.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/application-local.yaml")
 public class TasklistApplication {
 
     public static void main(String[] args) {
--- src/test/java/org/kurilov/tasklist/TasklistApplicationTests.java
+++ src/test/java/org/kurilov/tasklist/TasklistApplicationTests.java
@@ -2,8 +2,10 @@ package org.kurilov.tasklist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class TasklistApplicationTests {
 
     @Test
